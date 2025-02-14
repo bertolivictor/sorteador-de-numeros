64bd5e04e5352665dc8e10f36865b40b5c891846
Victor Bertoli <166616781+bertolivictor@users.noreply.github.com>
2025-02-13 22:02:23 -0300

    Finalizar Projeto

--- app.js
+++ app.js
@@ -1,54 +1,57 @@
-function sortear() {
+
+
+function sortear(){
     let quantidade = parseInt(document.getElementById('quantidade').value);
-    let de = parseInt(document.getElementById('de').value);
-    let ate = parseInt(document.getElementById('ate').value);
-    if(de >= ate) {
-        alert('ATENÇÃO! O campo "Do número" não pode ser maior que o campo "Até o número".');
-        limparCampos();
+    let deNumero = parseInt(document.getElementById('de').value);
+    let ateNumero = parseInt(document.getElementById('ate').value);
+
+// Protecao contra bug de loop infinito
+    if(ateNumero <= deNumero) {
+        alert('O valor do campo "Do número" não pode ser MAIOR que o valor do campo "Até o número".');
         return;
     }
-    if (quantidade > (ate - de +1)) {
-        alert('ATENÇÃO! O campo "Quantidade" não pode ser maior que os campos "Do número" e "Até o número".');
-        limparCampos();
+    if(quantidade > (ateNumero - deNumero) + 1) {
+        alert('A quantidade de números a serem sorteados não pode ser maior do que a quantidade de números contidos no intervalo a ser sorteado.');
         return;
     }
-    let sorteados = [];
-    let numero;
-    for(let i = 0; i < quantidade; i++) {
-        numero = obterNumeroAleatorio(de, ate);
-        while(sorteados.includes(numero)){
-            numero = obterNumeroAleatorio(de, ate);
+//--------------------------------------------------------
+
+    let numerosSorteados = [];
+    let numeroAleatorio;
+
+    for(i = 0; i < quantidade; i++) {
+        numeroAleatorio = gerarNumeroAleatorio(ateNumero, deNumero);
+        
+        while(numerosSorteados.includes(numeroAleatorio)) {
+            numeroAleatorio = gerarNumeroAleatorio(ateNumero, deNumero);
         }
-        sorteados.push(numero);
+        numerosSorteados.push(numeroAleatorio);
     }
     let resultado = document.getElementById('resultado');
-    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados} </label>`;
-    alterarStatusBotao();
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${numerosSorteados}</label>`;
+    trocarBotao();
 }
 
-function obterNumeroAleatorio(min, max) {
+function gerarNumeroAleatorio(max, min) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function reiniciar() {
-    limparCampos();
-    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>';
-    alterarStatusBotao();
-}
+function trocarBotao() {
+    let botaoReiniciar = document.getElementById('btn-reiniciar');
 
-function alterarStatusBotao() {
-    let reiniciar = document.getElementById('btn-reiniciar');
-    if(reiniciar.classList.contains('container__botao-desabilitado')) {
-        reiniciar.classList.remove('container__botao-desabilitado');
-        reiniciar.classList.add('container__botao');
-    } else {
-        reiniciar.classList.remove('container__botao');
-        reiniciar.classList.add('container__botao-desabilitado');
+    if(botaoReiniciar.classList.contains('container__botao-desabilitado')) {
+        botaoReiniciar.classList.remove('container__botao-desabilitado');
+        botaoReiniciar.classList.add('container__botao');
+    }else {
+        botaoReiniciar.classList.remove('container__botao');
+        botaoReiniciar.classList.add('container__botao-desabilitado');
     }
 }
 
-function limparCampos(){
+function reiniciar() {
     document.getElementById('quantidade').value = '';
     document.getElementById('de').value = '';
     document.getElementById('ate').value = '';
+    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>';
+    trocarBotao();
 }
